4a95b7fd9008d325871eb57f48e4db0d7a220761
Aman <ak669212@gmail.com>
2022-08-11 22:52:33 +0530

    header prallax

---
+++ script.js
@@ -0,0 +1,14 @@
+var bgscene = document.getElementById('bg-black');
+var bgparallax = new Parallax(bgscene);
+
+var btmstrp1 = document.getElementById('bottom-strp1');
+var strp1Parallax = new Parallax(btmstrp1);
+
+var btmstrp2 = document.getElementById('bottom-strp2');
+var strp1Parallax = new Parallax(btmstrp2);
+
+var btmstrp3 = document.getElementById('bottom-strp3');
+var strp3Parallax = new Parallax(btmstrp3);
+
+var btmstrp4 = document.getElementById('bottom-strp4');
+var strp4Parallax = new Parallax(btmstrp4);
